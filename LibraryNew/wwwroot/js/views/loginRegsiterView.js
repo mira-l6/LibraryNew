7397ab30fdb725a3656ed1fd4dee2680dd6d591c
mira-l6 <miralambreva19960@gmail.com>
2024-11-29 18:27:03 +0200

    логин страница заедно с регистрация, но още не са завършени анимациите и функционалността на два модела едновременно на една страница не работи (работят само по отделни страници)

---
+++ LibraryNew/wwwroot/js/views/loginRegsiterView.js
@@ -0,0 +1,33 @@
+﻿
+const loginSection = document.querySelector('.login-section');
+const registerSection = document.querySelector('.register-section');
+
+const loginButton = document.querySelector('#loginBtn');
+const registerButton = document.querySelector('#registerBtn');
+
+loginButton.addEventListener('click', signHandler);
+registerButton.addEventListener('click', signHandler);
+
+function signHandler(e) {
+
+    console.log('hjehrehreh')
+
+    if (e.currentTarget.getAttribute('id') == 'loginBtn') {
+
+        showHideSection(loginSection, registerSection);
+        loginButton.style.backgroundColor = '#d9d9d9';
+        registerButton.style.backgroundColor = '#f0f0f0';
+
+    } else if (e.currentTarget.getAttribute('id') == 'registerBtn') {
+
+        showHideSection(registerSection, loginSection);
+        registerButton.style.backgroundColor = '#d9d9d9';
+        loginButton.style.backgroundColor = '#f0f0f0';
+    }
+
+}
+
+function showHideSection(correct, hidden) {
+    correct.style.display = 'block';
+    hidden.style.display = 'none';
+}
